7a273817f69e1ede8b4ad05a53b1fcf7bd5f6277
skaper <1686048631@qq.com>
2020-10-08 14:52:47 +0800

    Updates

---
+++ precache-manifest.cc713c60aa0703c1858a5dca216751b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7928eb443b962e74b51aa2c1188f8f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "58bd8767ba13d8da86b6",
+    "url": "/react-app/static/css/main.9ddaa510.chunk.css"
+  },
+  {
+    "revision": "08c7c85176d72651583e",
+    "url": "/react-app/static/js/2.00e004ee.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/react-app/static/js/2.00e004ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bd8767ba13d8da86b6",
+    "url": "/react-app/static/js/main.a0d7ab05.chunk.js"
+  },
+  {
+    "revision": "3995c55679ee483357a6",
+    "url": "/react-app/static/js/runtime-main.39915a25.js"
+  }
+]);
